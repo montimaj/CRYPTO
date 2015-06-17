a8943cf467a40f07650094e852abca084707fded
montimaj <monti.majumdar@gmail.com>
2015-06-18 00:23:21 +0530

    Improved encryption level using bitwise columnar transpose

--- src/docrypto/Encrypt.java
+++ src/docrypto/Encrypt.java
@@ -4,11 +4,10 @@ import java.io.FileOutputStream;
 import java.util.Random;
 public class Encrypt 
 {
-	private static final int ncols=8;
+	private static int ncols=8;
 	private static void col_transpose(String s, int nrows, char mat[][], boolean flag[][])
 	{
 		int k=0;		
-		System.out.println(s);
 		for(int i=0;i<nrows;++i)
 		{
 			for(int j=0;j<ncols;++j)
@@ -57,6 +56,43 @@ public class Encrypt
 			System.out.println();
 		}
 	}
+	private static String cipher_to_bits(String cipher)
+	{
+		String bits="";
+		for(int i=0;i<cipher.length();++i)
+			bits+=Integer.toBinaryString((int)cipher.charAt(i));			
+		return bits;		
+	}
+	private static String generate_cipher(String key, int nrows, boolean flag[][], char mat[][])
+	{
+		StringBuilder cipher_text=new StringBuilder();
+		for(int i=0;i<key.length();++i)
+		{
+			int pos=(int)key.charAt(i)-48;
+			for(int j=0;j<nrows;++j)
+			{					
+				if(flag[j][pos])
+					cipher_text.append(mat[j][pos]);
+			}
+		}
+		return cipher_text.toString();
+	}
+	private static String bits_to_ascii(String bits)
+	{			
+		String ascii="";
+		for(int i=0;i<bits.length();)
+		{
+			int asc=0,j,power=6;
+			for(j=i;j<i+7 && j<bits.length();++j,--power)
+			{
+				int c=(int)bits.charAt(j)-48;
+				asc+=c*Math.pow(2, power);
+			}
+			i=j;
+			ascii+=(char)asc+" ";
+		}
+		return ascii;
+	}
 	public static void encrypt_file(String s)
 	{
 		try
@@ -67,24 +103,30 @@ public class Encrypt
 			char mat[][]=new char[nrows][ncols];
 			boolean flag[][]=new boolean[nrows][ncols];
 			col_transpose(s, nrows, mat, flag);
-			disp_mat(mat, flag, nrows);
 			String key=generate_key(ncols);	
 			FileOutputStream keyfos=new FileOutputStream("key.txt");
 			keyfos.write(key.getBytes());
 			keyfos.close();
-			StringBuilder cypher_text=new StringBuilder();
-			for(int i=0;i<key.length();++i)
-			{
-				int pos=(int)key.charAt(i)-48;
-				for(int j=0;j<nrows;++j)
-				{					
-					if(flag[j][pos])
-						cypher_text.append(mat[j][pos]);
-				}
-			}
-			FileOutputStream cos=new FileOutputStream("cypher_text.txt");
-			cos.write(cypher_text.toString().getBytes());		
+			String cipher=generate_cipher(key,nrows,flag,mat);
+			FileOutputStream cos=new FileOutputStream("cipher_text1.txt");
+			cos.write(cipher.getBytes());		
 			cos.close();
+			String bits=cipher_to_bits(cipher);
+			FileOutputStream bitstream=new FileOutputStream("cipher_bits.txt");			
+			bitstream.write(bits.getBytes());
+			bitstream.close();				
+			nrows=bits.length()/ncols;
+			if(bits.length()>(nrows*ncols))
+				nrows++;
+			char mat1[][]=new char[nrows][ncols];
+			boolean flag1[][]=new boolean[nrows][ncols];
+			col_transpose(bits, nrows, mat1, flag1);
+			cipher=generate_cipher(key,nrows,flag1,mat1);
+			cipher=bits_to_ascii(cipher);
+			cos=new FileOutputStream("cipher_text2.txt");
+			cos.write(cipher.getBytes());		
+			cos.close();	
+			disp_mat(mat1,flag1,nrows);
 		}
 		catch(Exception e)
 		{
